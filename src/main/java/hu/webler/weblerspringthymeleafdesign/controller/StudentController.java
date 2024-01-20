5d1d0744908aa7387f8875e32c3f0fcbd0cc6a34
webler-biology-physiology <csabavadasz79@gmail.com>
2024-01-20 17:16:29 +0100

    refactor logic in StudentController and create integration and unit test

--- src/main/java/hu/webler/weblerspringthymeleafdesign/controller/StudentController.java
+++ src/main/java/hu/webler/weblerspringthymeleafdesign/controller/StudentController.java
@@ -5,8 +5,10 @@ import hu.webler.weblerspringthymeleafdesign.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -16,8 +18,17 @@ public class StudentController {
 
     private final StudentService studentService;
 
-    @GetMapping({"", "/", "/home"}) // ({"", "/", "/home", "/index", "/index.html"})
+    @GetMapping("/students")
     public List<Student> renderStudents() {
         return studentService.getStudents();
+    }
+
+    @GetMapping("/students/search")
+    public List<Student> renderStudents(@RequestParam(required = false) String email) {
+        if (email != null && !email.isEmpty()) {
+            return studentService.searchStudentsByEmail(email);
+        } else {
+            return Collections.emptyList();
+        }
     }
 }
